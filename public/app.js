9117d35dfad24cafb209d4aa57730ee37f6d72d9
Wulfsbach <etemelkos@gmail.com>
2018-08-26 14:28:47 -0400

    halfway there i hope

---
+++ public/app.js
@@ -0,0 +1,29 @@
+$.getJSON("/articles", function(data){
+
+
+for (var i = 0; i < data.length; i++) {
+
+$("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+
+}
+
+});
+
+
+$(document).on("click", "p", function(){
+
+$("#notes").empty();
+
+var thisId= $(this).attr("data-id");
+
+
+$.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+})
+  .then(function(data){
+      console.log(data);
+  })
+
+
+})
